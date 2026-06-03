23dae49c5fe1857bb0e0556d5829e18604593340
Lucas G. Reinert <lucasreinert96@gmail.com>
2026-06-03 08:33:11 -0300

    Ajustes - removendo tabela de preços

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,6 @@ function Header() {
       <div className={`fixed top-0 right-0 w-64 transform ${isMenuOpen ? 'translate-x-0' : 'translate-x-full'} bg-[#FF7E95] h-full transition-transform duration-300 shadow-lg z-20 md:relative md:translate-x-0 md:w-auto md:h-auto md:bg-transparent md:shadow-none`}>
         <ul className="flex flex-col md:flex-row md:items-center md:justify-start md:text-2xl text-white pt-5 md:pt-0 md:space-x-5">
           <li><Link legacyBehavior href="/"><a className={`block w-full text-center md:text-left border-b md:border-none border-white p-3 md:p-0 ${pathname === '/' ? 'active' : ''} hover:text-gray-400`}>Início</a></Link></li>
-          <li><Link legacyBehavior href="/precos"><a className={`block w-full text-center md:text-left border-b md:border-none border-white p-3 md:p-0 ${pathname === '/precos' ? 'active' : ''} hover:text-gray-400`}>Tabela de preços</a></Link></li>
           <li><Link legacyBehavior href="/galeria"><a className={`block w-full text-center md:text-left border-b md:border-none border-white p-3 md:p-0 ${pathname === '/galeria' ? 'active' : ''} hover:text-gray-400`}>Galeria</a></Link></li>
           <li><Link legacyBehavior href="/contato"><a className={`block w-full text-center md:text-left p-3 md:p-0 md:pr-5 ${pathname === '/contato' ? 'active' : ''} hover:text-gray-400`}>Contato</a></Link></li>
         </ul>
